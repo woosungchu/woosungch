7059fe8ea4d556ed71f49f79a9336d9920132e11
woosungchu <woosungchu@gmail.com>
2017-05-01 23:54:53 +0900

    first commit

--- application/__init__.py
+++ application/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 import os
 
 #init
-app = Flask(__name__)
+application = Flask(__name__)
 
 #Global Variables
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT,'static/')
 
-@app.route('/')
+@application.route('/')
 def hello():
-    return render_template('app.html')
+    return render_template('index.html')
